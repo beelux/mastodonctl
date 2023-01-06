dbfb8c0652fe3b3a9902f91808868ecb84f98d72
socraticDevBlog <maximebonin@outlook.com>
2023-01-06 14:28:53 -0500

    refactor decouple topics stuff

--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/fatih/color"
 	"github.com/rodaine/table"
@@ -34,81 +31,6 @@ func (conf *Conf) DefaultConf() {
 func PrettyPrint(i interface{}) string {
 	s, _ := json.MarshalIndent(i, "", "\t")
 	return string(s)
-}
-
-type Response []struct {
-	ID                 string      `json:"id"`
-	CreatedAt          time.Time   `json:"created_at"`
-	InReplyToID        interface{} `json:"in_reply_to_id"`
-	InReplyToAccountID interface{} `json:"in_reply_to_account_id"`
-	Sensitive          bool        `json:"sensitive"`
-	SpoilerText        string      `json:"spoiler_text"`
-	Visibility         string      `json:"visibility"`
-	Language           string      `json:"language"`
-	URI                string      `json:"uri"`
-	URL                string      `json:"url"`
-	RepliesCount       int         `json:"replies_count"`
-	ReblogsCount       int         `json:"reblogs_count"`
-	FavouritesCount    int         `json:"favourites_count"`
-	EditedAt           interface{} `json:"edited_at"`
-	Content            string      `json:"content"`
-	Reblog             interface{} `json:"reblog"`
-	Account            struct {
-		ID             string        `json:"id"`
-		Username       string        `json:"username"`
-		Acct           string        `json:"acct"`
-		DisplayName    string        `json:"display_name"`
-		Locked         bool          `json:"locked"`
-		Bot            bool          `json:"bot"`
-		Discoverable   bool          `json:"discoverable"`
-		Group          bool          `json:"group"`
-		CreatedAt      time.Time     `json:"created_at"`
-		Note           string        `json:"note"`
-		URL            string        `json:"url"`
-		Avatar         string        `json:"avatar"`
-		AvatarStatic   string        `json:"avatar_static"`
-		Header         string        `json:"header"`
-		HeaderStatic   string        `json:"header_static"`
-		FollowersCount int           `json:"followers_count"`
-		FollowingCount int           `json:"following_count"`
-		StatusesCount  int           `json:"statuses_count"`
-		LastStatusAt   string        `json:"last_status_at"`
-		Emojis         []interface{} `json:"emojis"`
-		Fields         []interface{} `json:"fields"`
-	} `json:"account"`
-	MediaAttachments []struct {
-		ID               string      `json:"id"`
-		Type             string      `json:"type"`
-		URL              string      `json:"url"`
-		PreviewURL       string      `json:"preview_url"`
-		RemoteURL        string      `json:"remote_url"`
-		PreviewRemoteURL interface{} `json:"preview_remote_url"`
-		TextURL          interface{} `json:"text_url"`
-		Meta             struct {
-			Original struct {
-				Width  int     `json:"width"`
-				Height int     `json:"height"`
-				Size   string  `json:"size"`
-				Aspect float64 `json:"aspect"`
-			} `json:"original"`
-			Small struct {
-				Width  int     `json:"width"`
-				Height int     `json:"height"`
-				Size   string  `json:"size"`
-				Aspect float64 `json:"aspect"`
-			} `json:"small"`
-		} `json:"meta"`
-		Description interface{} `json:"description"`
-		Blurhash    string      `json:"blurhash"`
-	} `json:"media_attachments"`
-	Mentions []interface{} `json:"mentions"`
-	Tags     []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"tags"`
-	Emojis []interface{} `json:"emojis"`
-	Card   interface{}   `json:"card"`
-	Poll   interface{}   `json:"poll"`
 }
 
 func main() {
@@ -168,7 +90,6 @@ func main() {
 				tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
 
 				for _, r := range accounts {
-
 					tbl.AddRow(r.ID, r.UserName, r.DisplayName, r.URL, r.FollowersCount, r.FollowingCount)
 				}
 
@@ -195,18 +116,8 @@ func main() {
 					return nil
 				}
 
-				uri := fmt.Sprintf("%s/api/v1/timelines/tag/%s?limit=%d", conf.ApiUrl, hashtag, conf.ResultsDisplayCount)
-
-				resp, err := http.Get(uri)
+				results, err := GetHashtag(InTopics{Hashtag: hashtag, ApiUrl: conf.ApiUrl, ResultsCount: conf.ResultsDisplayCount})
 				if err != nil {
-					log.Fatal(err)
-				}
-				defer resp.Body.Close()
-
-				body, err := ioutil.ReadAll(resp.Body) // response body is []byte
-
-				var results Response
-				if err := json.Unmarshal(body, &results); err != nil { // Parse []byte to go struct pointer
 					log.Fatal(err)
 				}
 
@@ -217,13 +128,7 @@ func main() {
 				tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
 
 				for _, r := range results {
-					url := "{no media}"
-
-					if len(r.MediaAttachments) > 0 {
-						url = r.MediaAttachments[len(r.MediaAttachments)-1].URL
-					}
-
-					tbl.AddRow(hashtag, r.Account.Username, url)
+					tbl.AddRow(r.Hashtag, r.Username, r.MediaURL)
 				}
 
 				tbl.Print()
---
+++ topics_api.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type InTopics struct {
+	Hashtag      string
+	ApiUrl       string
+	ResultsCount int
+}
+
+func GetHashtag(in InTopics) ([]OutTopics, error) {
+	uri := fmt.Sprintf("%s/api/v1/timelines/tag/%s?limit=%d", in.ApiUrl, in.Hashtag, in.ResultsCount)
+
+	resp, err := http.Get(uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	var results Response
+	if err := json.Unmarshal(body, &results); err != nil { // Parse []byte to go struct pointer
+	}
+
+	var ret []OutTopics
+	for _, r := range results {
+		var n OutTopics
+		n.Hashtag = in.Hashtag
+		n.Username = r.Account.Username
+
+		if len(r.MediaAttachments[len(r.MediaAttachments)-1].URL) > 0 {
+			n.MediaURL = r.MediaAttachments[len(r.MediaAttachments)-1].URL
+		} else {
+			n.MediaURL = "{no media}"
+		}
+
+		ret = append(ret, n)
+	}
+
+	return ret, err
+}
+
+type OutTopics struct {
+	MediaURL string
+	Username string
+	Hashtag  string
+}
+
+type Response []struct {
+	ID                 string      `json:"id"`
+	CreatedAt          time.Time   `json:"created_at"`
+	InReplyToID        interface{} `json:"in_reply_to_id"`
+	InReplyToAccountID interface{} `json:"in_reply_to_account_id"`
+	Sensitive          bool        `json:"sensitive"`
+	SpoilerText        string      `json:"spoiler_text"`
+	Visibility         string      `json:"visibility"`
+	Language           string      `json:"language"`
+	URI                string      `json:"uri"`
+	URL                string      `json:"url"`
+	RepliesCount       int         `json:"replies_count"`
+	ReblogsCount       int         `json:"reblogs_count"`
+	FavouritesCount    int         `json:"favourites_count"`
+	EditedAt           interface{} `json:"edited_at"`
+	Content            string      `json:"content"`
+	Reblog             interface{} `json:"reblog"`
+	Account            struct {
+		ID             string        `json:"id"`
+		Username       string        `json:"username"`
+		Acct           string        `json:"acct"`
+		DisplayName    string        `json:"display_name"`
+		Locked         bool          `json:"locked"`
+		Bot            bool          `json:"bot"`
+		Discoverable   bool          `json:"discoverable"`
+		Group          bool          `json:"group"`
+		CreatedAt      time.Time     `json:"created_at"`
+		Note           string        `json:"note"`
+		URL            string        `json:"url"`
+		Avatar         string        `json:"avatar"`
+		AvatarStatic   string        `json:"avatar_static"`
+		Header         string        `json:"header"`
+		HeaderStatic   string        `json:"header_static"`
+		FollowersCount int           `json:"followers_count"`
+		FollowingCount int           `json:"following_count"`
+		StatusesCount  int           `json:"statuses_count"`
+		LastStatusAt   string        `json:"last_status_at"`
+		Emojis         []interface{} `json:"emojis"`
+		Fields         []interface{} `json:"fields"`
+	} `json:"account"`
+	MediaAttachments []struct {
+		ID               string      `json:"id"`
+		Type             string      `json:"type"`
+		URL              string      `json:"url"`
+		PreviewURL       string      `json:"preview_url"`
+		RemoteURL        string      `json:"remote_url"`
+		PreviewRemoteURL interface{} `json:"preview_remote_url"`
+		TextURL          interface{} `json:"text_url"`
+		Meta             struct {
+			Original struct {
+				Width  int     `json:"width"`
+				Height int     `json:"height"`
+				Size   string  `json:"size"`
+				Aspect float64 `json:"aspect"`
+			} `json:"original"`
+			Small struct {
+				Width  int     `json:"width"`
+				Height int     `json:"height"`
+				Size   string  `json:"size"`
+				Aspect float64 `json:"aspect"`
+			} `json:"small"`
+		} `json:"meta"`
+		Description interface{} `json:"description"`
+		Blurhash    string      `json:"blurhash"`
+	} `json:"media_attachments"`
+	Mentions []interface{} `json:"mentions"`
+	Tags     []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"tags"`
+	Emojis []interface{} `json:"emojis"`
+	Card   interface{}   `json:"card"`
+	Poll   interface{}   `json:"poll"`
+}
